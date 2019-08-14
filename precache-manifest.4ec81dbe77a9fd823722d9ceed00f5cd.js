07ae463557c54bf0243a5a2da6fecd12dba28e3d
Jrej <jrejoire@hotmail.fr>
2019-08-14 06:07:15 +0200

    Updates

---
+++ precache-manifest.4ec81dbe77a9fd823722d9ceed00f5cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c92d0e8781368d2cb03a257ae8ed96",
+    "url": "/starwars_fanapp/index.html"
+  },
+  {
+    "revision": "f3c0e9210365154eb197",
+    "url": "/starwars_fanapp/static/css/main.af20131a.chunk.css"
+  },
+  {
+    "revision": "76f358f233d108bd3aed",
+    "url": "/starwars_fanapp/static/js/2.38cb3ddd.chunk.js"
+  },
+  {
+    "revision": "f3c0e9210365154eb197",
+    "url": "/starwars_fanapp/static/js/main.5e6c7bb0.chunk.js"
+  },
+  {
+    "revision": "add2f04f69e20e9e7d02",
+    "url": "/starwars_fanapp/static/js/runtime~main.46a79da6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/starwars_fanapp/precache-manifest.f876fa5cd4f47ad2c3d53b930851f7e1.js"
+  "/starwars_fanapp/precache-manifest.4ec81dbe77a9fd823722d9ceed00f5cd.js"
 );
 
 self.addEventListener('message', (event) => {
